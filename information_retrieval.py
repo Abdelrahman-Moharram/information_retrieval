8ddcbadb205edae84ee318bd9a3f93476570cac1
Abdelrahman Moharram <41553398+Abdelrahman-Moharram@users.noreply.github.com>
2021-12-21 06:29:29 +0200

    fix null input bug and and print with good view

--- information_retrieval.py
+++ information_retrieval.py
@@ -1,16 +1,30 @@
 import os
 import re
-import glob
-import nltk
-from nltk.corpus import stopwords
-from nltk import word_tokenize
-import string
-from collections import Counter
 import numpy as np
-from collections import OrderedDict
 from tabulate import tabulate
 
+
+
+
+
+
 ##################################              utils               ####################################
+
+def read_files(path):
+    list_files = []
+    files = os.listdir(path)
+    for file in files:
+        filename = os.fsdecode(file)
+        with open(path+"/"+filename) as f:
+            Str = ''
+            for a in f.read().split("\n"):
+                if a:
+                    Str += a
+            list_files.append(Str)
+            Str = ''
+
+    return list_files, files
+
 def word_str_count(word, string):
     if word not in string:
         return 0
@@ -41,20 +55,7 @@ def preprocess(list_files):
 
 #########################################################################################################
 
-def read_files(path):
-    list_files = []
-    files = os.listdir(path)
-    for file in files:
-        filename = os.fsdecode(file)
-        with open(path+"/"+filename) as f:
-            Str = ''
-            for a in f.read().split("\n"):
-                if a:
-                    Str += a
-            list_files.append(Str)
-            Str = ''
-
-    return list_files, files
+##################################              phase-2               ####################################
 
 def positionalIndex(list_files):
     indexs = {}
@@ -74,7 +75,6 @@ def positionalIndex(list_files):
     return indexs, indexs_iteration
 
 def phrase_query(indexs, text, files_names):
-    rows = []
     list_result = {}
     indexs = indexs[0]
     for file in indexs:
@@ -99,9 +99,10 @@ def phrase_query(indexs, text, files_names):
     #     if file not in list_result:
     #         list_result[file] = "Not Availble !"
             
-    print("list_result: ",list_result)
     return list_result
+#########################################################################################################
 
+##################################              phase-3               ####################################
 
 def DocFreq(list_files, unique_words):
     df = {}
@@ -156,6 +157,20 @@ def tfidf(weight, idf):
 
 
 def Control(list_files, files_names):
+    txt = clear_str(input("--> ")).lower()
+    text = txt.split()
+    if len(text) < 2:  
+        print("\nplease enter 2 words or more!\n")
+        return Control(list_files, files_names)
+    flag = True
+    for file in list_files:
+        if  txt in " ".join(file):
+            flag = False
+    if flag:
+         print("\nthis text not available please try again with different words !\n")
+         return Control(list_files, files_names)
+    
+    
     len_files = len(files_names)
     if len_files > 1:
         print(len_files, " Files Found")
@@ -164,28 +179,23 @@ def Control(list_files, files_names):
     else:
         return "\nNo Files in this directory\n"
     ##################################          get text from user      ####################################
-    
-    txt = input("--> ")
-    for p in "!.,:@#$%^&?<>*()[}{]-=;/\"\\\t\n":
-            String = ''
-            if p in '\n;?:!.,.':
-                txt = txt.replace(p,' ')
-            else:
-                txt = txt.replace(p,'')
-    txt = remove_white_spaces(txt)
-    text = txt.lower().split()
-    if len(text) < 2:  
-        print("\nplease enter 2 words or more!\n")
-        return Control(list_files, files_names)
+
+
     
     print("\nMaking Positional Index ...\n")
     indexs = positionalIndex(list_files)
     
     #########################################################################################################
     phrase = phrase_query(indexs, text, files_names)
-    for index in phrase:
-        print("-->",index, " : ",phrase[index])
-    print("\n")
+    # for index in phrase:
+    #     print("-->",index, " : ",phrase[index])
+    # print("\n")
+    print("_"*150, "\n\t\t\t\t\t\t\t\tphrase query\n","_"*150,"\n")
+    
+    view_list = []
+    for x  in phrase:
+        view_list.append([x, phrase[x]])
+    print(tabulate(view_list, headers=['word', 'phrase'], tablefmt='orgtbl'), "\n\n\n")
     return indexs, phrase, txt
 
 def query(txt, idf):
@@ -196,8 +206,7 @@ def query(txt, idf):
         tf_idf[w] = idf[w] * weights[w]
     for word in tf_idf:
         sum += tf_idf[word]
-    print("\n\n\nquery_weights",weights,"\n\nquery_tf_idf",tf_idf,"\nquery_tf_idf_sum:", sum)
-    return tf_idf, sum
+    return weights, tf_idf, sum
 
 def vectorSpaceModel(tf_dict, idf, s):
     file_tf_weight = {}
@@ -207,7 +216,7 @@ def vectorSpaceModel(tf_dict, idf, s):
     for file in tf_dict:
         f_weight = Weights(tf_dict[file])
         file_tf_weight[file] = f_weight
-    print("\n\n\nf_weight ",file_tf_weight," \n\n")
+    # print("\n\n\nf_weight ",file_tf_weight," \n\n")
     
     for file in file_tf_weight:
         file_tf_idf[file] = {}
@@ -219,11 +228,34 @@ def vectorSpaceModel(tf_dict, idf, s):
     
     for w in files_weights_sums:
         similarity_doc_query[w] = files_weights_sums[w] * s
-    print("\n\n\nfile_tf_idf ",file_tf_idf," \n\n files_weights_sums: ", files_weights_sums, "\n\nsimilarity_doc_query: ",similarity_doc_query)
+    # print("\n\n\nfile_tf_idf ",file_tf_idf," \n\n files_tfidf_sums: ", files_weights_sums, "\n\nsimilarity_doc_query: ",similarity_doc_query)
     
     return file_tf_weight, file_tf_idf, files_weights_sums, similarity_doc_query
         
 
+
+def printer(title,unique_words, files):
+    print("_"*150, "\n\t\t\t\t\t\t\t\t", title,"\n","_"*150,"\n")
+    
+    files_names = ["word"]
+    
+    for file in files:
+        files_names.append(file)
+    row = []
+    rows = []
+    for word in unique_words:
+        
+        row = []
+        row.append(word)
+        for file in files:
+            if word in files[file]:
+                row.append(files[file][word])
+            else:
+                row.append(0)
+        rows.append(row)
+    print(tabulate(rows, headers=files_names, tablefmt='orgtbl'),"\n\n")
+#########################################################################################################
+        
 
 def controller(list_files, files_names):
     p_index, phrase, txt = Control(list_files, files_names)
@@ -231,52 +263,71 @@ def controller(list_files, files_names):
     for file in list_files:
         docs +=file
     
-    print("docs: ", docs,"\n\nlist_files : ",list_files,"\n\nfiles_names: ", files_names,"\n\npositional index: ", p_index,"\n\nPhrase : ", phrase)
+    # print("docs: ", docs,"\n\nlist_files : ",list_files,"\n\nfiles_names: ", files_names,"\n\npositional index: ", p_index,"\n\nPhrase : ", phrase)
     
     
     
     
     
     # M = len(docs)
+    # print("\n\n=>docs: ", docs)
 
     unique_words = list(set(docs))
     
     tf = termFrequency(docs)
-    print("\n\n=>tf: ", tf)
     
     tf_dict = termFrequencyInDoc([list_files,files_names])
-    print("\n\n\n\ntf_dict: ",tf_dict)
+    printer("tf_dict",unique_words, tf_dict)
     weight = Weights(tf) 
-    print("\n\n\nWeights: ",weight)
     
 
     # print("\n\n\ntf_idf: ",tf_idf,"\n\n\n")
     df = DocFreq(list_files, unique_words)
-    print("\n\n\n\n\n\ndf",df)
     idf = inverseDocFre(files_names, df)
-    print("\n\n\n\nidf",idf,"\n")
-    query_tf_idf, s = query(txt, idf)
-    file_tf_weight, file_tf_idf, files_weights_sums, similarity_doc_query = vectorSpaceModel(tf_dict, idf, s)
+    query_weights, query_tf_idf, query_sum = query(txt, idf)
+    file_tf_weight, file_tf_idf, files_weights_sums, similarity_doc_query = vectorSpaceModel(tf_dict, idf, query_sum)
+    printer("file tf weight",unique_words, file_tf_weight)
     tf_idf = tfidf(weight, idf) 
-    print("\n\n\n\ntf_idf",tf_idf,"\n")
+    # print("\n\n\n\ntf_idf",tf_idf,"\n")
+    # print("\n\n\nWeights: ",weight)
     
+    # print("\n\n\nfiles_weights_sums: ",files_weights_sums)
+    # print("\n\n\nsimilarity_doc_query: ",similarity_doc_query,"\n\n")
+    
+
+
+
+    
+    # for file in file_tf_weight:
+    #     view_list = []
+    #     print("_"*100,"\n\n",file,"\n")
+    #     for x  in file_tf_weight[file]:
+    #         view_list.append([x, file_tf_weight[file][x], file_tf_idf[file][x], tf_dict[file][x]])
+    #     print(tabulate(view_list, headers=['word', 'df_dict','idf_dict', 'tf_dict'], tablefmt='orgtbl'), "\n\n",files_weights_sums[file], " * ", s, " = ",  similarity_doc_query[file],"\n","_"*100)
+    
+    print("_"*150, "\n\t\t\t\t\t\t\t\tdf - idf\n","_"*150,"\n")
     
     view_list = []
     for x  in tf:
-        view_list.append([x, tf[x], weight[x], idf[x]])
-    print(tabulate(view_list, headers=['word', 'tf','Weights', 'idf'], tablefmt='orgtbl'))
+        view_list.append([x, df[x], idf[x]])
+    print(tabulate(view_list, headers=['word', 'df','idf'], tablefmt='orgtbl'),"\n\n\n")
     
-    for file in file_tf_weight:
-        view_list = []
-        print("_"*100,"\n\n",file,"\n")
-        for x  in file_tf_weight[file]:
-            view_list.append([x, file_tf_weight[file][x], file_tf_idf[file][x], tf_dict[file][x]])
-        print(tabulate(view_list, headers=['word', 'df_dict','idf_dict', 'tf_dict'], tablefmt='orgtbl'), "\n\n",files_weights_sums[file], " * ", s, " = ",  similarity_doc_query[file],"\n","_"*100)
+    
+    printer("file tf idf",unique_words, file_tf_idf)
+    
+    print("query_weights",query_weights,"\n\nquery_tf_idf",query_tf_idf,"\nquery_tf_idf_sum:", query_sum,"\n\n\n")
+
+    view_list = []
+    for x  in files_weights_sums:
+        view_list.append([x, files_weights_sums[x]])
+    print(tabulate(view_list, headers=['word', 'files_weights_sums'], tablefmt='orgtbl'), "\n\n\n")
     
 
+    view_list = []
+    for x  in similarity_doc_query:
+        view_list.append([x, str(files_weights_sums[x]) + " * " + str(query_sum) + " = "+ str(similarity_doc_query[x])])
+    print(tabulate(view_list, headers=['word', 'similarity_doc_query'], tablefmt='orgtbl'), "\n\n\n")
 
 files = read_files("files")
 
-
 controller(preprocess(files[0]), files[1])
-# control1(files)
